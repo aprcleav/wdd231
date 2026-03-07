4a2df1aee1d698c1d91c68f54c0547e68a43f430
aprcleav <aprcleaver@gmail.com>
2026-03-06 16:20:00 -0800

    W1 Course Home Page progress

---
+++ scripts/date.js
@@ -0,0 +1,7 @@
+
+//Footer Date
+const year = document.querySelector("#currentyear");
+const today = new Date();
+
+year.innerHTML = today.getFullYear()
+document.getElementById("lastModified").innerHTML = document.lastModified;
--- week 1/scripts/navigation.js
+++ week 1/scripts/navigation.js
@@ -2,14 +2,11 @@
 
 const navButton = document.querySelector('#ham-button');
 const navBar = document.querySelector('#nav-bar');
-const current = document.querySelector('.current');
 
 navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
     navBar.classList.toggle('show');
 });
-
-
 
 
 
