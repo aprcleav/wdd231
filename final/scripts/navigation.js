87779547b18b5dd46f83e5d2e09e1c3e4187cd63
aprcleav <aprcleaver@gmail.com>
2026-04-06 11:22:24 -0700

    Getting started on final project website

---
+++ final/scripts/navigation.js
@@ -0,0 +1,10 @@
+// Hamburger & Nav Bar
+
+const navButton = document.querySelector('#ham-button');
+const navBar = document.querySelector('#nav-bar');
+const navLinks = document.querySelectorAll('#nav-bar li');
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
+});
